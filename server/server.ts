4bee4c2da1c88f8afbb5d003e8861fe50a9c1495
rishabh150805 <rishabhsrivastava1508@gmail.com>
2026-08-02 18:41:37 +0530

    created API for restaurants

--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,7 @@ import express, { Request, Response } from 'express';
 import cors from "cors";
 import connectDB from "./config/db.js";
 import authRouter from './routes/authRoutes.js';
+import restaurantRouter from './routes/restaurantRoutes.js';
 
 const app = express();
 
@@ -20,6 +21,8 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
 app.use("/api/auth", authRouter); 
+app.use("/api/restaurants", restaurantRouter); 
+
 
 //Global error Handler
 app.use((err: Error, req: Request, res: Response, next: Function) => {
